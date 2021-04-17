3bf78bb4c0763488c58d10160c6022f6eb786479
Eskindir <eskindiraleb@yahoo.com>
2021-04-17 00:39:58 -0500

    helloworld

---
+++ demo/src/main/java/project4/EventLogApplication.java
@@ -0,0 +1,13 @@
+package project4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventLogApplication.class, args);
+	}
+
+}
